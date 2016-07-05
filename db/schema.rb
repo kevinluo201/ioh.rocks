9c0c53b7f0816f969171f12936a586f0585621a0
arfullight <tmimvhuang@gmail.com>
2016-07-06 05:15:30 +0800

    rails g migration add_feedback

--- db/schema.rb
+++ db/schema.rb
@@ -11,19 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160705172612) do
+ActiveRecord::Schema.define(version: 20160705211318) do
 
   create_table "live_departments", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.integer  "group",      limit: 4
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.string  "name",  limit: 255
+    t.integer "group", limit: 4
   end
 
   create_table "live_schools", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.string "name", limit: 255
   end
 
   create_table "live_times", force: :cascade do |t|
@@ -43,8 +39,8 @@ ActiveRecord::Schema.define(version: 20160705172612) do
 
   create_table "lives", force: :cascade do |t|
     t.string   "name",               limit: 255
-    t.datetime "created_at",                     null: false
-    t.datetime "updated_at",                     null: false
+    t.datetime "created_at",                            null: false
+    t.datetime "updated_at",                            null: false
     t.integer  "live_school_id",     limit: 4
     t.integer  "live_department_id", limit: 4
     t.string   "title",              limit: 255
@@ -55,6 +51,7 @@ ActiveRecord::Schema.define(version: 20160705172612) do
     t.boolean  "stream_201602"
     t.string   "location",           limit: 255
     t.string   "ioh_url",            limit: 255
+    t.text     "feedback",           limit: 4294967295
   end
 
   add_index "lives", ["live_department_id"], name: "index_lives_on_live_department_id", using: :btree

4f576b50a89e24c82ea0ae6fcfcb30cefe4df9c1
arfullight <arfullight@ioh.tw>
2016-07-05 17:42:21 -0400

    change move.rb

--- db/schema.rb
+++ db/schema.rb
@@ -14,16 +14,12 @@
 ActiveRecord::Schema.define(version: 20160705172612) do
 
   create_table "live_departments", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.integer  "group",      limit: 4
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.string  "name",  limit: 255
+    t.integer "group", limit: 4
   end
 
   create_table "live_schools", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.string "name", limit: 255
   end
 
   create_table "live_times", force: :cascade do |t|

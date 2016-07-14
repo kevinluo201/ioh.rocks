be3488f9791e219724555ab0365d8e9bdf095821
arfullight <tmimvhuang@gmail.com>
2016-07-14 17:46:14 +0800

    add db/seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,61 +1,57 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-
-
-r1 = Role.create({name: "Regular", description: "Can read items"})
-r2 = Role.create({name: "Editor", description: "Can read and create items. Can update and destroy own items"})
-r3 = Role.create({name: "Admin", description: "Can perform any CRUD operation on any resource"})
-
-times = LiveTime.create([
-		{start: DateTime.strptime("07/18/2016 6:00", "%m/%d/%Y %H:%M")},
-		{start: DateTime.strptime("07/18/2016 6:30", "%m/%d/%Y %H:%M")},
-		{start: DateTime.strptime("07/18/2016 7:00", "%m/%d/%Y %H:%M")},
-		{start: DateTime.strptime("07/18/2016 7:30", "%m/%d/%Y %H:%M")},
-		{start: DateTime.strptime("07/18/2016 8:00", "%m/%d/%Y %H:%M")},
-		{start: DateTime.strptime("07/18/2016 12:00", "%m/%d/%Y %H:%M")},
-		{start: DateTime.strptime("07/18/2016 12:30", "%m/%d/%Y %H:%M")},
-		{start: DateTime.strptime("07/18/2016 13:00", "%m/%d/%Y %H:%M")},
-		{start: DateTime.strptime("07/18/2016 13:30", "%m/%d/%Y %H:%M")},
-		{start: DateTime.strptime("07/18/2016 14:00", "%m/%d/%Y %H:%M")},
-
-		{start: DateTime.strptime("07/19/2016 6:00", "%m/%d/%Y %H:%M")},
-		{start: DateTime.strptime("07/19/2016 6:30", "%m/%d/%Y %H:%M")},
-		{start: DateTime.strptime("07/19/2016 7:00", "%m/%d/%Y %H:%M")},
-		{start: DateTime.strptime("07/19/2016 7:30", "%m/%d/%Y %H:%M")},
-		{start: DateTime.strptime("07/19/2016 8:00", "%m/%d/%Y %H:%M")},
-		{start: DateTime.strptime("07/19/2016 12:00", "%m/%d/%Y %H:%M")},
-		{start: DateTime.strptime("07/19/2016 12:30", "%m/%d/%Y %H:%M")},
-		{start: DateTime.strptime("07/19/2016 13:00", "%m/%d/%Y %H:%M")},
-		{start: DateTime.strptime("07/19/2016 13:30", "%m/%d/%Y %H:%M")},
-		{start: DateTime.strptime("07/19/2016 14:00", "%m/%d/%Y %H:%M")},
-
-		{start: DateTime.strptime("07/20/2016 6:00", "%m/%d/%Y %H:%M")},
-		{start: DateTime.strptime("07/20/2016 6:30", "%m/%d/%Y %H:%M")},
-		{start: DateTime.strptime("07/20/2016 7:00", "%m/%d/%Y %H:%M")},
-		{start: DateTime.strptime("07/20/2016 7:30", "%m/%d/%Y %H:%M")},
-		{start: DateTime.strptime("07/20/2016 8:00", "%m/%d/%Y %H:%M")},
-		{start: DateTime.strptime("07/20/2016 12:00", "%m/%d/%Y %H:%M")},
-		{start: DateTime.strptime("07/20/2016 12:30", "%m/%d/%Y %H:%M")},
-		{start: DateTime.strptime("07/20/2016 13:00", "%m/%d/%Y %H:%M")},
-		{start: DateTime.strptime("07/20/2016 13:30", "%m/%d/%Y %H:%M")},
-		{start: DateTime.strptime("07/20/2016 14:00", "%m/%d/%Y %H:%M")},
+schools = LiveSchool.create([
+  { name: "嶺東科技大學" },
+  { name: "馬偕醫學院" },
+  { name: "聯合大學" },
+  { name: "真理大學" },
+  { name: "德明科技大學" },
+  { name: "中信金融管理學院" }
 ])
 
-=begin
-u1 = User.create({account_name: "Sally", email: "sally@example.com", password: "aaaaaaaa", password_confirmation: "aaaaaaaa", role_id: r1.id})
-u2 = User.create({account_name: "Sue", email: "sue@example.com", password: "aaaaaaaa", password_confirmation: "aaaaaaaa", role_id: r2.id})
-u3 = User.create({account_name: "Kev", email: "kev@example.com", password: "aaaaaaaa", password_confirmation: "aaaaaaaa", role_id: r2.id})
-u4 = User.create({account_name: "Jack", email: "jack@example.com", password: "aaaaaaaa", password_confirmation: "aaaaaaaa", role_id: r3.id})
+departments = LiveDepartment.create([
+	{ name: "美術與文創學系" },
+	{ name: "能源工程學系" },
+	{ name: "巨量資料管理學系" },
+	{ name: "教育經營與管理學系" }
+])
 
-i1 = Poster.create({name: "Rayban Sunglasses", description: "Stylish shades", user_id: u2.id})
-i2 = Poster.create({name: "Gucci watch", description: "Expensive timepiece", user_id: u2.id})
-i3 = Poster.create({name: "Henri Lloyd Pullover", description: "Classy knitwear", user_id: u3.id})
-i4 = Poster.create({name: "Porsche socks", description: "Cosy footwear", user_id: u3.id})
-=end
+
+# r1 = Role.create({name: "Regular", description: "Can read items"})
+# r2 = Role.create({name: "Editor", description: "Can read and create items. Can update and destroy own items"})
+# r3 = Role.create({name: "Admin", description: "Can perform any CRUD operation on any resource"})
+
+# times = LiveTime.create([
+# 		{start: DateTime.strptime("07/18/2016 6:00", "%m/%d/%Y %H:%M")},
+# 		{start: DateTime.strptime("07/18/2016 6:30", "%m/%d/%Y %H:%M")},
+# 		{start: DateTime.strptime("07/18/2016 7:00", "%m/%d/%Y %H:%M")},
+# 		{start: DateTime.strptime("07/18/2016 7:30", "%m/%d/%Y %H:%M")},
+# 		{start: DateTime.strptime("07/18/2016 8:00", "%m/%d/%Y %H:%M")},
+# 		{start: DateTime.strptime("07/18/2016 12:00", "%m/%d/%Y %H:%M")},
+# 		{start: DateTime.strptime("07/18/2016 12:30", "%m/%d/%Y %H:%M")},
+# 		{start: DateTime.strptime("07/18/2016 13:00", "%m/%d/%Y %H:%M")},
+# 		{start: DateTime.strptime("07/18/2016 13:30", "%m/%d/%Y %H:%M")},
+# 		{start: DateTime.strptime("07/18/2016 14:00", "%m/%d/%Y %H:%M")},
+
+# 		{start: DateTime.strptime("07/19/2016 6:00", "%m/%d/%Y %H:%M")},
+# 		{start: DateTime.strptime("07/19/2016 6:30", "%m/%d/%Y %H:%M")},
+# 		{start: DateTime.strptime("07/19/2016 7:00", "%m/%d/%Y %H:%M")},
+# 		{start: DateTime.strptime("07/19/2016 7:30", "%m/%d/%Y %H:%M")},
+# 		{start: DateTime.strptime("07/19/2016 8:00", "%m/%d/%Y %H:%M")},
+# 		{start: DateTime.strptime("07/19/2016 12:00", "%m/%d/%Y %H:%M")},
+# 		{start: DateTime.strptime("07/19/2016 12:30", "%m/%d/%Y %H:%M")},
+# 		{start: DateTime.strptime("07/19/2016 13:00", "%m/%d/%Y %H:%M")},
+# 		{start: DateTime.strptime("07/19/2016 13:30", "%m/%d/%Y %H:%M")},
+# 		{start: DateTime.strptime("07/19/2016 14:00", "%m/%d/%Y %H:%M")},
+
+# 		{start: DateTime.strptime("07/20/2016 6:00", "%m/%d/%Y %H:%M")},
+# 		{start: DateTime.strptime("07/20/2016 6:30", "%m/%d/%Y %H:%M")},
+# 		{start: DateTime.strptime("07/20/2016 7:00", "%m/%d/%Y %H:%M")},
+# 		{start: DateTime.strptime("07/20/2016 7:30", "%m/%d/%Y %H:%M")},
+# 		{start: DateTime.strptime("07/20/2016 8:00", "%m/%d/%Y %H:%M")},
+# 		{start: DateTime.strptime("07/20/2016 12:00", "%m/%d/%Y %H:%M")},
+# 		{start: DateTime.strptime("07/20/2016 12:30", "%m/%d/%Y %H:%M")},
+# 		{start: DateTime.strptime("07/20/2016 13:00", "%m/%d/%Y %H:%M")},
+# 		{start: DateTime.strptime("07/20/2016 13:30", "%m/%d/%Y %H:%M")},
+# 		{start: DateTime.strptime("07/20/2016 14:00", "%m/%d/%Y %H:%M")},
+# ])
